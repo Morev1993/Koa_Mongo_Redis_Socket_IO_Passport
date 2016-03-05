304cd4e666adf2c80b511fb243a6fa1dcfbb35d4
Oleg Lustenko <diethxbye@gmail.com>
2016-03-05 09:09:01 +0200

    instal sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-npm i --save -D koa-favicon koa-static koa-logger koa-bodyparser koa-generic-session 
-npm i --save -D koa-views jade koa-router config co-busboy koa-passport passport-local cookies socket.io socket.io-emitter socket.io-redis
-npm i --save -D passport-facebook passport-vkontakte
+npm i  -D koa-favicon koa-static koa-logger koa-bodyparser koa-generic-session 
+npm i  -D koa-views jade koa-router config co-busboy koa-passport passport-local cookies socket.io socket.io-emitter socket.io-redis
+npm i  -D passport-facebook passport-vkontakte
 
